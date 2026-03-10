c826c1c30a7b5cbef5b82e687d26687d89a620f6
airhine <taeho7702@gmail.com>
2026-03-10 20:42:55 +0900

    db 다시 작동

--- src/models/index.js
+++ src/models/index.js
@@ -13,7 +13,7 @@ const {
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
-  port: DB_PORT || 3306,
+  port: DB_PORT,
   dialect: 'mysql',
   logging: false
 });
